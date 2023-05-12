71eb23887be89bb3e1ea9915472b7699e564354d
Camron Naderi <camronnaderi57@gmail.com>
2023-05-12 16:53:34 -0700

    added test file for eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
-/**
- ==> Make a function "eqArrays" which takes in two arrays and returns true or false, based on a perfect match!
-
- Step 1: Turn the arrays in to strings so they can compared(maybe using .join?)
- Step 2: Use if statement comparing those two strings.
- Step 3: Return statement!
- */
 const eqArrays = function(array1, array2) {
 
   if (!array1 || !array2 || array1.length !== array2.length) {
@@ -30,7 +14,4 @@ const eqArrays = function(array1, array2) {
 
 };
 
-
-assertEqual(eqArrays([1, 2, 3], [3, 2, 2]), false); // => should PASS
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays
--- head.js
+++ head.js
@@ -2,7 +2,7 @@ function head(array) {
   if (array.length === 0) {
     return undefined;
   }
-   return array[0]
+   return array[0];
 }
 
 module.exports = head;
